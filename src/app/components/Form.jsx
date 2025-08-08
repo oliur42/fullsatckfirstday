e209aabcf7794b808ae3b4e18b1e52bc82a98546
oliur42 <bmolyahmed42@gmail.com>
2025-08-08 12:22:24 +0600

    Initial commit from Create Next App

---
+++ src/app/components/Form.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <div>
+      <div className='w-[1250px] mx-auto'>
+        <h1>Hello welcome to my website</h1>
+
+      </div>
+    </div>
+  )
+}
+
+export default Form
